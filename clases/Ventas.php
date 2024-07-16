6427629c08d9a8b565b7f2c7f646810bf0d13491
Genesis Diaz <genesismazariegos006@gmail.com>
2024-07-16 14:04:11 -0600

    Tarea de POO usando PHP

---
+++ clases/Ventas.php
@@ -0,0 +1,19 @@
+<?php
+    include_once('Empleado.php');
+    class Ventas extends Empleado {
+        public $tipoContratacion="";
+        public $comisionVenta=0;
+        
+        public function calcularSueldoLiquido(){
+            $sueldoLiquido = parent::calcularSueldoLiquido() + $this->comisionVenta;
+            return $sueldoLiquido;
+        } 
+        public function verDatos(){
+            return parent::verDatos() 
+            ."<br>Tipo Contratacion: " 
+            . $this->tipoContratacion 
+            . "<br>Comisión: " 
+            . $this->comisionVenta;
+        }
+    }
+?>
